4e346c6b6fe6f8537030dd509ec5b6e27d9e1276
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-22 03:50:12 +0530

    [7] Experinces added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Experience from "./components/Experience"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
@@ -18,6 +19,7 @@ const App = () => {
         <Hero />
         <Technologies />
         <Projects />
+        <Experience />
       </div>
     </div>
   )
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,48 @@
+import { EXPERIENCES } from "../constants";
+
 const Experience = () => {
     return (
-        <div></div>
-    )
-}
+        <div className="pb-4 relative z-50 px-6">
+            <h2 className="my-20 text-center text-4xl font-bold text-white">
+                Experience
+            </h2>
 
-export default Experience
+            <div className="max-w-5xl mx-auto">
+                {EXPERIENCES.map((experience, index) => (
+                    <div key={index} className="mb-12 flex flex-col lg:flex-row lg:justify-between">
+
+                        {/* Year (Left Side) */}
+                        <div className="w-full lg:w-1/4 mb-2 lg:mb-0">
+                            <p className="text-sm text-stone-400">{experience.year}</p>
+                        </div>
+
+                        {/* Experience Details (Right Side) */}
+                        <div className="w-full lg:w-3/4 space-y-2">
+                            <h3 className="text-lg font-semibold text-white">
+                                {experience.role} -{" "}
+                                <span className="text-sm text-stone-500">{experience.company}</span>
+                            </h3>
+
+                            <p className="text-stone-400 leading-relaxed">
+                                {experience.description}
+                            </p>
+
+                            {/* Tech Stack Badges */}
+                            <div className="flex flex-wrap mt-3">
+                                {experience.technologies.map((tech, index) => (
+                                    <span
+                                        key={index}
+                                        className="mr-2 mt-2 rounded bg-stone-900 px-3 py-1 text-sm font-medium text-stone-300">
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Experience;
