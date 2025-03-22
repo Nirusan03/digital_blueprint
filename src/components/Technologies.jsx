c326a9c47d10666123642abb6e340f5bd0a9f259
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-22 14:56:43 +0530

    [10] Frame motion on tech header added

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -2,11 +2,34 @@ import { DiRedis } from "react-icons/di"
 import { FaNodeJs } from "react-icons/fa"
 import { RiReactjsLine } from "react-icons/ri"
 import { SiAmazonecs, SiAngular, SiApachekafka, SiDocker, SiFastapi, SiFlask, SiJenkins, SiMongodb, SiMysql, SiSpring, SiSpringboot, SiVuedotjs } from "react-icons/si"
+import { motion } from "framer-motion"
+
+const iconVariants = (duration) => ({
+    initial: {y: -10},
+    animate: {
+        y: [10, -10],
+        transition:{
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        }
+    }
+})
 
 const Technologies = () => {
     return(
         <div className="pb-24 relative z-50 px-6">
-            <h2 className="my-20 text-center text-4xl"> Technologies</h2>
+
+            <motion.h2
+                whileInView={{opacity: 1, y:0}}
+                initial={{opacity:0, y:-100}}
+                transition={{duration:1.5}}
+                className="my-20 text-center text-4xl"> 
+                
+                Technologies
+            </motion.h2>
+            
             <div className="flex flex-wrap items-center justify-center gap-4">
                 <div>
                     <SiSpringboot className="text-7xl text-green-500" />
