357fb241d55a1c42c60d0234e11b52e9ddfb3815
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-22 14:48:49 +0530

    [10] Frame motion added for Hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,23 @@
 import profilePic from "../assets/nirusanHariharan.webp";
 import { MY_CONTENT } from "../constants";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+    hidden: {opacity: 0, x: -100},
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            duration: 0.5,
+            staggerChildren: 0.5,
+        }
+    }
+}
+
+const childVariants = {
+    hidden: {opacity: 0, x: -100},
+    visible: {opacity: 1, x:0, transition: {duration: 0.5}}
+}
 
 const Hero = () => {
     return (
@@ -9,36 +27,54 @@ const Hero = () => {
                 {/* Image Container */}
                 <div className="w-full lg:w-1/2">
                     <div className="flex justify-center lg:p-8">
-                        <img src={profilePic} 
+                        <motion.img src={profilePic} 
                              alt="Nirusan Hariharan" 
-                             className="border border-stone-900 rounded-3xl w-[650px] h-[650px] object-cover shadow-lg" />
+                             className="border border-stone-900 rounded-3xl w-[650px] h-[650px] object-cover shadow-lg"
+                             width={650}
+                             height={650}
+                             initial={{x:100, opacity:0}}
+                             animate={{x:0, opacity: 1}}
+                             transition={{duration: 1, delay: 1.5}}
+                             />
                     </div>
                 </div>
 
                 {/* Text Content */}
                 <div className="w-full lg:w-1/2">
-                    <div className="flex flex-col items-center lg:items-start mt-10">
+                    <motion.div 
+                        initial="hidden"
+                        animate="visible"
+                        variants={containerVariants}
+                        className="flex flex-col items-center lg:items-start mt-10">
                         
-                        <h2 className="pb-2 text-4xl tracking-tighter lg:text-6xl">
+                        <motion.h2
+                            variants={childVariants} 
+                            className="pb-2 text-4xl tracking-tighter lg:text-6xl">
                             Nirusan Hariharan
-                        </h2>
+                        </motion.h2>
                         
-                        <span className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">
+                        <motion.span
+                            variants={childVariants}
+                            className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">
                             Software Engineer
-                        </span>
+                        </motion.span>
 
-                        <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
+                        <motion.p
+                            variants={childVariants}
+                            className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
                             {MY_CONTENT}
-                        </p>
+                        </motion.p>
 
-                        <a href="/Nirusan_Resume.pdf" 
-                           target="_blank" 
-                           rel="noopener noreferrer" 
-                           download 
-                           className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">
+                        <motion.a
+                            variants={childVariants}
+                            href="/Nirusan_Resume.pdf" 
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            download
+                            className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">
                             Download Resume
-                        </a>
-                    </div>
+                        </motion.a>
+                    </motion.div>
                 </div>
             </div>
         </div>
