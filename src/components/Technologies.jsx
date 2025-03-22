2cbdd4244ca3435ad79f7e3200889caaaf36a420
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-22 15:18:59 +0530

    [10] Framemotion added for technologies component

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -30,68 +30,132 @@ const Technologies = () => {
                 Technologies
             </motion.h2>
             
-            <div className="flex flex-wrap items-center justify-center gap-4">
-                <div>
+            <motion.div 
+                whileInView={{opacity: 1, x:0}}
+                initial={{opacity:0, x: -100}}
+                transition={{duration: 1.5}}
+                className="flex flex-wrap items-center justify-center gap-4">
+                
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(2.5)}>
                     <SiSpringboot className="text-7xl text-green-500" />
-                </div>
+                </motion.div>
 
-                <div className="p-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(3)}
+                    className="p-4">
                     <SiSpring className="text-7xl text-green-500" />
-                </div>
+                </motion.div>
 
-                <div className="p-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(5)}
+                    className="p-4">
                     <FaNodeJs className="text-7xl text-green-500" />
-                </div>
+                </motion.div>
 
-                <div className="p-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(2)}
+                    className="p-4">
                     <SiFastapi className="text-7xl text-teal-500" />
-                </div>
+                </motion.div>
 
-                <div className="p-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(6)}
+                    className="p-4">
                     <SiFlask className="text-7xl text-teal-500" />
-                </div>
+                </motion.div>
 
-                <div className="p-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(4)}
+                    className="p-4">
                     <RiReactjsLine className="text-7xl text-cyan-400" />
-                </div>
+                </motion.div>
 
-                <div className="p-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(2.5)}
+                    className="p-4">
                     <SiAngular className="text-7xl text-red-500" />
-                </div>
+                </motion.div>
 
-                <div className="p-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(3)}
+                    className="p-4">
                     <SiVuedotjs className="text-7xl text-green-400" />
-                </div>
+                </motion.div>
 
-                <div className="p-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(5)}
+                    className="p-4">
                     <DiRedis className="text-7xl text-red-700" />
-                </div>
+                </motion.div>
 
-                <div className="p-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(2)}
+                    className="p-4">
                     <SiMysql className="text-7xl text-blue-500" />
-                </div>
+                </motion.div>
 
-                <div className="p-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(6)}
+                    className="p-4">
                     <SiMongodb className="text-7xl text-cyan-500" />
-                </div>
+                </motion.div>
 
-                <div className="p-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(4)}
+                    className="p-4">
                     <SiDocker className="text-7xl text-blue-500" />
-                </div>
+                </motion.div>
 
-                <div className="p-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(2.5)}
+                    className="p-4">
                     <SiAmazonecs className="text-7xl text-orange-500" />
-                </div>
+                </motion.div>
 
-                <div className="p-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(3)}
+                    className="p-4">
                     <SiJenkins className="text-7xl text-red-500" />
-                </div>
+                </motion.div>
 
-                <div className="p-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(3)}
+                    className="p-4">
                     <SiApachekafka className="text-7xl text-white-500" />
-                </div>
+                </motion.div>
 
-            </div>
+            </motion.div>
         </div>
     )
 }
