c1cf550a9a6fed2e773929bfe5180cbde0e6945a
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-22 16:05:36 +0530

    [10] Framemotion added for experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,23 +1,41 @@
 import { EXPERIENCES } from "../constants";
+import { motion } from "framer-motion";
 
 const Experience = () => {
     return (
         <div className="pb-4 relative z-50 px-6">
-            <h2 className="my-20 text-center text-4xl font-bold text-white">
+            
+            <motion.h2
+                whileInView={{opacity:1, y:0}}
+                initial={{opacity:0, y: -100}}
+                transition={{duration:0.5}}
+                className="my-20 text-center text-4xl font-bold text-white">
+
                 Experience
-            </h2>
+
+            </motion.h2>
 
             <div className="max-w-5xl mx-auto">
                 {EXPERIENCES.map((experience, index) => (
                     <div key={index} className="mb-12 flex flex-col lg:flex-row lg:justify-between">
 
-                        {/* Year (Left Side) */}
-                        <div className="w-full lg:w-1/4 mb-2 lg:mb-0">
-                            <p className="text-sm text-stone-400">{experience.year}</p>
-                        </div>
+                        <motion.div
+                            whileInView={{opacity: 1, x: 0}}
+                            initial={{opacity:0, x:-100}}
+                            transition={{duration: 1}}
+                            className="w-full lg:w-1/4 mb-2 lg:mb-0">
 
-                        {/* Experience Details (Right Side) */}
-                        <div className="w-full lg:w-3/4 space-y-2">
+                            <p className="text-sm text-stone-400">
+                                {experience.year}
+                            </p>
+
+                        </motion.div>
+
+                        <motion.div
+                            whileInView={{opacity: 1, x:0}}
+                            initial={{opacity:0, x: 100}}
+                            transition={{duration: 1}}
+                            className="w-full lg:w-3/4 space-y-2">
                             <h3 className="text-lg font-semibold text-white">
                                 {experience.role} -{" "}
                                 <span className="text-sm text-stone-500">{experience.company}</span>
@@ -37,7 +55,7 @@ const Experience = () => {
                                     </span>
                                 ))}
                             </div>
-                        </div>
+                        </motion.div>
                     </div>
                 ))}
             </div>
